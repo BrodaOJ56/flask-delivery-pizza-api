4c873977752af2aaeea44c2f89617588f20e078d
OLUBUNMI OLUWATOBI JAMES <82912148+BrodaOJ56@users.noreply.github.com>
2023-02-23 00:05:12 +0100

    latest updates

--- api/config/config.py
+++ api/config/config.py
@@ -3,8 +3,7 @@ from decouple import config
 from datetime import timedelta
 
 
-BASE_DIR=os.path.dirname(os.path.realpath(__file__))
-
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 import os
 import re
@@ -16,11 +15,11 @@ if uri.startswith("postgres://"):
 
 
 class Config:
-    SECRET_KEY=config('SECRET_KEY','Secret')
-    JWT_ACCESS_TOKEN_EXPIRES=timedelta(minutes=30)
-    JWT_REFRESH_TOKEN_EXPIRES=timedelta(minutes=30)
-    JWT_SECRET_KEY=config('JWT_SECRET_KEY')
-    
+    SECRET_KEY = config('SECRET_KEY', 'Secret')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
+    JWT_SECRET_KEY = config('JWT_SECRET_KEY')
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI="sqlite:///"+os.path.join(BASE_DIR,'db.sqlite3')
@@ -28,22 +27,20 @@ class DevConfig(Config):
     SQLALCHEMY_ECHO=True
     DEBUG=True
 
-
-
 class TestConfig(Config):
-    TESTING=True
-    SQLALCHEMY_DATABASE_URI="sqlite://"
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
-    SQLALCHEMY_ECHO=True
+    TESTING = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI=uri
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     DEBUG=config('DEBUG',cast=bool)
 
- 
-config_dict={
-    'dev':DevConfig,
-    'testing':TestConfig,
-    'production':ProdConfig
+
+config_dict = {
+    'dev': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
 }
